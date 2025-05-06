734333281a9274191386c1b56d445415b6df0269
shibinantony <40263130+shibinantony@users.noreply.github.com>
2025-05-06 12:52:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,8 +65,8 @@ def index():
     </head>
     <body>
         <div class="container">
-            <h1>Shibin is working on it.</h1>
-            <p>Check back later for updates!</p>
+            <h1>Test page by Shibin Antony Boban.</h1>
+            <p>Proof of Concept for Webapp deployment on Azure</p>
 
             <iframe width="784" height="441" src="https://www.youtube-nocookie.com/embed/AyzJyWGqm0c?si=oEckVtcRg3zhFTwT&amp;start=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
